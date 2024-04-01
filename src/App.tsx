dea3c7bf97ccbfae03e31a8ada05fe162a9f483c
iqbalsyahbana <it.programmer2@triatra.co.id>
2024-04-01 10:05:41 +0700

    push

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React PWA
+          Learn React PWA Mobile Android
         </a>
       </header>
     </div>
