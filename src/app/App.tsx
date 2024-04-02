9bada8f95319926679f8b6356fa7516edd5c9bc0
iqbalsyahbana <it.programmer2@triatra.co.id>
2024-04-02 14:26:16 +0700

    push data

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import ChatbotComponent from "./components/ChatbotComponent";
+
+function App() {
+  return (
+    <div className="min-h-screen">
+      <ChatbotComponent />
+    </div>
+  );
+}
+
+export default App;
