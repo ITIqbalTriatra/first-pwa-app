a84aa323c2423ee2e9c215dcd33185ab60c72f8d
iqbalsyahbana <it.programmer2@triatra.co.id>
2024-04-03 11:02:04 +0700

    add ant design

--- src/app/bot/widgets/options/DateOptions.tsx
+++ src/app/bot/widgets/options/DateOptions.tsx
@@ -2,22 +2,19 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { AppDispatch } from "../../../redux/store";
 import { startCount } from "../../../redux/features/messages-slice";
+import type { DatePickerProps } from 'antd';
+import { DatePicker } from 'antd';
 
 const DateOptions: React.FC<any> = (props) => {
   const dispatch = useDispatch<AppDispatch>();
 
-  const handleDate = (e: React.ChangeEvent<HTMLInputElement>) => {
-    props.actionProvider.handleUserInput(parseInt(e.target.value));
+  const handleDate: DatePickerProps['onChange'] = (date, dateString) => {
+    props.actionProvider.handleUserInput(date);
     dispatch(startCount());
   };
 
   return (
-    <input
-      className="bg-gray-100 rounded p-2"
-      type="date"
-      placeholder="Select Date"
-      onChange={handleDate}
-    />
+    <DatePicker onChange={handleDate} />
   );
 };
 
